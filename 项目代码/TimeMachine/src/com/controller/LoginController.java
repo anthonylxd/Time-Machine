e7e62aa6f40dcf142e712fa3caf2b84e0aedab58
anthonylxd <lxdblues@gmail.com>
2018-09-22 13:45:24 +0800

    201809221

--- 项目代码/TimeMachine/src/com/controller/LoginController.java
+++ 项目代码/TimeMachine/src/com/controller/LoginController.java
@@ -3,16 +3,21 @@ package com.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.alibaba.fastjson.JSON;
+import com.service.LoginService;
 
 @Controller
 @RequestMapping(value="/login")
 public class LoginController {
+	
+	@Autowired
+	private LoginService loginService;
 	
 	@RequestMapping(value="/login")
 	public ModelAndView login(){
@@ -26,9 +31,31 @@ public class LoginController {
 	public String loginuser(String parname,String password){
 		
 		Map map = new HashMap();
-		System.out.println("222 "+password);
+		System.out.println("222 "+password);		
+		if (!loginService.checkuser(parname)) {
+			map.put("result", "fail");
+			map.put("login", "1");
+		}else if(!loginService.loginuser(parname, password)){
+			map.put("result", "fail");
+			map.put("login", "2");
+		}else{
+			map.put("result", "success");
+			map.put("login", "0");
+		}
+				
+		/**
+		 * result
+		 * 正常登录 success
+		 * 登录失败 fail
+		 */		
+		/**
+		 * login
+		 * 0 正常登录
+		 * 1 用户名不存在
+		 * 2 密码错误
+		 * 3 其它异常
+		 */
 		
-		map.put("result", "success");
 		return JSON.toJSONString(map);
 	}
 	
